b36a2f329429ca58cb35fefa94a5fc69538a546a
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-09-02 13:24:35 +0300

    Test Update

--- src/edit.js
+++ src/edit.js
@@ -41,7 +41,7 @@ export default function Edit( { attributes, setAttributes } ) {
 				post_tag: tagsId,
 			} );
 		},
-		[ numberOfPosts, order, orderBy, categories, tags ]
+		[ numberOfPosts, order, orderBy, catIds, tagsId ]
 	);
 
 	// Get all the categories.
